c793d0e4d39b5d4ec151146c79cd623ad35a95d9
Gautam goel <gautamgoel96.2@gmail.com>
2022-10-21 19:31:39 +0530

    Created a complete rest api, model, hope it add value.

---
+++ rest_api/tut2.js
@@ -0,0 +1,10 @@
+// What is a rest api?
+// representational state transfer, application programming interface. 
+// It is a software that allows two application to communicate with each other over the internet, and through various devices. Every time you access an app like facebook or check the weather on your smartphone, an api is used. 
+// A restful api is an architectural style, for an api that uses http request to access and use data. rest is not a programming language. 
+// we need to perform crud operations in api. 
+// create: POST /api/users
+// read: GET /api/users
+// update: PUT PATCH /api/users
+// delete: DELETE /api/users 
+// the end point will always be same. 
